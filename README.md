f4aea18a67ddf7fe9df78285dba2d03fd31925b9
Dominic Humphries <djh@thermeon.eu>
2013-03-29 14:47:09 +0000

    Initial commit of boilerplate from Dist::Milla

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+App::Goto - Blah blah blah
+
+# SYNOPSIS
+
+    use App::Goto;
+
+# DESCRIPTION
+
+App::Goto is
+
+# AUTHOR
+
+Dominic Humphries <dominic@oneandoneis2.com>
+
+# COPYRIGHT
+
+Copyright 2013- Dominic Humphries
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/App/Goto.pm
@@ -0,0 +1,39 @@
+package App::Goto;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::Goto - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use App::Goto;
+
+=head1 DESCRIPTION
+
+App::Goto is
+
+=head1 AUTHOR
+
+Dominic Humphries E<lt>dominic@oneandoneis2.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2013- Dominic Humphries
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
