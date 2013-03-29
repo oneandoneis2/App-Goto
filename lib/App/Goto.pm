42ceef0ff9d6794999b6aaf5a27f23aa3d8ae6f2
Dominic Humphries <djh@thermeon.eu>
2013-03-29 16:28:50 +0000

    Initial commit of working script, object & test

--- lib/App/Goto.pm
+++ lib/App/Goto.pm
@@ -10,8 +10,50 @@ use Config::Tiny;
 has args        => ( is => 'ro', required => 1 );
 has config      => ( is => 'ro', required => 1 );
 has error       => ( is => 'rw', default => 'Unknown error' );
-has is_success  => ( is => 'rw' );
+has is_success  => ( is => 'rw', default => 1 );
 has cmd         => ( is => 'rw' );
+
+sub BUILD {
+    my $self = shift;
+    my ($server, $command) = @{$self->args};
+
+    my $hostname = $self->_get_host($server);
+    return unless $hostname;
+
+    my $remote_command = '';
+    if ($command) {
+        $remote_command = $self->_get_command($command);
+        }
+
+    $self->cmd( "ssh $hostname $remote_command" );
+    }
+
+sub _get_host {
+    my ($self, $name) = @_;
+    # Get known hosts from config
+    my $hosts = $self->config->{hosts};
+    # Get the first hostname from a sorted list of hosts
+    my ($hostname) = grep { $_ =~ m#^$name# } sort keys %{$hosts};
+    unless ($hostname) {
+        $self->is_success(0);
+        $self->error('Cannot find hostname in config') unless $hostname;
+        return;
+        }
+    # Get the right server details for the found hostname
+    return $hosts->{$hostname};
+    }
+
+sub _get_command {
+    my ($self, $cmd) = @_;
+    my $commands = $self->config->{commands};
+    my $command = $commands->{$cmd};
+    unless ($command) {
+        $self->is_success(0);
+        $self->error('Command not recognised') unless $command;
+        return;
+        }
+    return "-t $command";
+    }
 
 1;
 __END__
--- t/basic.t
+++ t/basic.t
@@ -6,7 +6,8 @@ use Config::Tiny;
 # Setup a default config
 my $config = bless( {
         'hosts' => {
-            'localhost' => '127.0.0.1'
+            'localhost' => '127.0.0.1',
+            'lozzzzzzz' => 'Z'
             },
         'commands' => {
             'log' => '\'cd /var/log/ && bash\''
@@ -14,8 +15,28 @@ my $config = bless( {
         }, 'Config::Tiny' );
 
 CREATE: {
-    my $goto = App::Goto->new({ config => $config, args => [] });
+    my $goto = App::Goto->new({ config => $config, args => [qw/lo/] });
     is(ref $goto, 'App::Goto', 'Got a valid object');
-    }
+    };
+
+GET_RIGHT_AMBIGUOUS_HOSTNAME: {
+    my $goto = App::Goto->new({ config => $config, args => [qw/lo/] });
+    ok($goto->cmd() =~ m#127\.0\.0\.1#, 'Got the right ambiguous server');
+    };
+
+GET_RIGHT_NONAMBIGUOUS_HOSTNAME: {
+    my $goto = App::Goto->new({ config => $config, args => [qw/loz/] });
+    ok($goto->cmd() =~ m#Z#, 'Got the right non-ambiguous server');
+    };
+
+GET_RIGHT_COMMAND: {
+    my $goto = App::Goto->new({ config => $config, args => [qw/lo log/] });
+    ok($goto->cmd() =~ m#var/log#, 'Got the right command');
+    };
+
+GET_RIGHT_COMMAND: {
+    my $goto = App::Goto->new({ config => $config, args => [qw/lo log/] });
+    is($goto->cmd(), "ssh 127.0.0.1 -t 'cd /var/log/ && bash'", 'Everything looks good');
+    };
 
 done_testing;
