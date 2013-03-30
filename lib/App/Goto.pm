8ee17ff8d6e404e072d031de00612c5326659f46
Dominic Humphries <djh@thermeon.eu>
2013-03-30 09:14:39 +0000

    Update default Moo attrs

--- lib/App/Goto.pm
+++ lib/App/Goto.pm
@@ -9,9 +9,9 @@ use Moo;
 has args        => ( is => 'ro', required => 1 );
 has config      => ( is => 'ro', required => 1 );
 has error       => ( is => 'rw', default => sub { 'Unknown error' } );
-has is_success  => ( is => 'rw', default => 1 );
+has is_success  => ( is => 'rw', default => sub { 1 } );
 has cmd         => ( is => 'rw' );
-has name        => ( is => 'rw', default => '' );
+has name        => ( is => 'rw', default => sub { '' } );
 
 sub BUILD {
     my $self = shift;
